52abd07ca85f5aff9cce839116c0f30edef5b030
Scorppb <scorppb@yandex.ru>
2022-09-20 19:47:59 +0300

    CreateArray

--- Program.cs
+++ Program.cs
@@ -13,3 +13,26 @@ void PrintArray(string[] array)
     }
     System.Console.WriteLine();
 }
+
+string[] CreateArray()
+{   
+    int count = 0;
+    string value = "";
+    string[] tempArray = new string[1000];
+    System.Console.WriteLine($"Для завершения ввода введите 'exit'");
+    for (count = 0; value != "exit"; count++)
+    {
+        value = Prompt($"Введите значение элемента {count+1}> ");
+        if (value != "exit") 
+        {
+            tempArray[count] = value;
+        }
+    }
+    string[] array = new string[count-1];
+    for (int i = 0; i < array.Length; i++)
+    {
+         array[i] = tempArray[i];
+    }
+    return array;
+}
+
